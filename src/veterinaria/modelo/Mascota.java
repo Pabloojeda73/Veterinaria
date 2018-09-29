29ba1a9cbd22854cbdb7cce79b265f9d2501cab6
Ojeda <pabloojeda73@gmail.com>
2018-09-29 18:51:08 -0300

    clases datas agregadas

--- src/veterinaria/modelo/Mascota.java
+++ src/veterinaria/modelo/Mascota.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package veterinaria;
+package veterinaria.modelo;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
