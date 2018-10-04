fc858c5312a79499c69f9dfdbaec878d3909700c
Martin Ochoa <martincho8a@gmail.com>
2018-10-03 21:48:36 -0300

    Se agrega constructor y getters/setters a Tratamiento y guardarTratamiento a TratamientoData

--- src/veterinaria/modelo/Tratamiento.java
+++ src/veterinaria/modelo/Tratamiento.java
@@ -16,4 +16,61 @@ public class Tratamiento {
     private double importe;
     private boolean activo;
 
+    public Tratamiento() {
+        this.id = -1;
+    }
+
+    public Tratamiento(String tipo, String descripcion, double importe, boolean activo) {
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.importe = importe;
+        this.activo = activo;
+    }
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String toString() {
+        return this.id + " - " + this.tipo;
+    }
+    
+
 }
